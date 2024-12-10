23ed5ccca6f5ac062dea601e74ec6398cfccb63e
Nowherenew <63805209+Nowherenew@users.noreply.github.com>
2024-12-10 08:14:22 +0700

    Update question.js

--- question.js
+++ question.js
@@ -68,7 +68,7 @@ function displayCatHeart() {
         document.getElementById('wait').style.display = 'contents';
         document.getElementById('hehe').style.display = 'contents';
     };
-    var audio = new Audio("/audio.mp3");
+    var audio = new Audio("audio.mp3");
     audio.play();
     setTimeout(function () {
         window.location.href = "/meley191299/flower.html";
